a47b32e2a57584a16e7255bfe3397028521e01ab
rivacortezf2006 <u202215313@upc.edu.pe>
2024-12-30 15:59:27 -0500

    feat(categories, notes): implement CategoriesService and NotesService with CRUD operations and association methods

--- Frontend/src/app/notes/services/categories.service.ts
+++ Frontend/src/app/notes/services/categories.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { BaseService } from '../../shared/services/base.service';
+import { Observable } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+import { CategoriesEntity } from '../model/categories.entity';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CategoriesService {
+export class CategoriesService extends BaseService<CategoriesEntity> {
 
-  constructor() { }
+  protected override resourceEndpoint = '/categories';
+
+  associateCategoryWithNote(categoryId: number, noteId: number): Observable<void> {
+    return this.http.post<void>(`${this.basePath}${this.resourceEndpoint}/${categoryId}/notes/${noteId}`, {}, this.httOptions)
+      .pipe(retry(2), catchError(this.handleError));
+  }
+
+  disassociateCategoryFromNote(categoryId: number, noteId: number): Observable<void> {
+    return this.http.delete<void>(`${this.basePath}${this.resourceEndpoint}/${categoryId}/notes/${noteId}`, this.httOptions)
+      .pipe(retry(2), catchError(this.handleError));
+  }
+
+  archiveCategory(id: number): Observable<void> {
+    return this.http.patch<void>(`${this.basePath}${this.resourceEndpoint}/${id}/archive`, {}, this.httOptions)
+      .pipe(retry(2), catchError(this.handleError));
+  }
 }
--- Frontend/src/app/notes/services/notes.service.ts
+++ Frontend/src/app/notes/services/notes.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { BaseService } from '../../shared/services/base.service';
+import { Observable } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+import {NotesEntity} from '../model/notes.entity';
 
 @Injectable({
   providedIn: 'root'
 })
-export class NotesService {
+export class NotesService extends BaseService<NotesEntity> {
 
-  constructor() { }
+  protected override resourceEndpoint = '/notes';
+
+  associateNoteWithCategory(noteId: number, categoryId: number): Observable<void> {
+    return this.http.post<void>(`${this.basePath}${this.resourceEndpoint}/${noteId}/categories/${categoryId}`, {}, this.httOptions)
+      .pipe(retry(2), catchError(this.handleError));
+  }
+
+  disassociateNoteFromCategory(noteId: number, categoryId: number): Observable<void> {
+    return this.http.delete<void>(`${this.basePath}${this.resourceEndpoint}/${noteId}/categories/${categoryId}`, this.httOptions)
+      .pipe(retry(2), catchError(this.handleError));
+  }
+
+  archiveNote(id: number): Observable<void> {
+    return this.http.patch<void>(`${this.basePath}${this.resourceEndpoint}/${id}/archive`, {}, this.httOptions)
+      .pipe(retry(2), catchError(this.handleError));
+  }
 }
